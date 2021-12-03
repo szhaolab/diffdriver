7d408505d5457eab6392d619eb6204530579fca6
atvorundunn <47759175+atvorundunn@users.noreply.github.com>
2021-12-02 19:58:00 -0500

    Add ddmodel back in

--- R/diffdriver.R
+++ R/diffdriver.R
@@ -103,7 +103,7 @@ diffdriver <- function(genef, mutf, phenof, drivermapsdir, outputdir =".", outpu
     fe <- as.matrix(ganno[ ,names(betaf), with =F]) %*% betaf + betaf0
 
     resg <- list()
-#   resg[["dd"]] <- ddmodel(mutmtx, canno$Phenotype, bmrmtx, fe[,1])
+    resg[["dd"]] <- ddmodel(mutmtx, canno$Phenotype, bmrmtx, fe[,1])
     resg[["mlr"]] <- mlr(mutmtx, canno$Phenotype)
     resg[["mlr.v2"]] <- mlr.v2(mutmtx, canno$Phenotype, canno$Nsyn)
 #   resg[["fisher"]] <- genefisher(mutmtx, canno$Phenotype)
