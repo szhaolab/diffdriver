4971e364176dc335fd65a494d17bf01453923866
simingz <michellezhao.student@gmail.com>
2022-03-21 19:30:01 -0400

    fix plot

--- R/plots.R
+++ R/plots.R
@@ -6,9 +6,14 @@ plot_mut <- function(mutmtx, canno, bmrmtx, ganno){
   df <- rbind(canno$Phenotype,colSums(mutmtx), colSums(mutmtx[ganno$functypecode8>0,]), colSums(mutmtx[ganno$mycons>0,]),colSums(exp(bmrmtx)))
   rownames(df) <- c("Phenotype", "Nonsyn", "LoF", "Cons.", "BMR")
   df <- df[,order(df["Phenotype",])]
-  df <- df[,order(df["BMR",])]
   par(mfrow=c(dim(df)[1],1), mar=c(0.5,5,0.5,1))
-  barplot(rep(1,length(df[1,])), col=c("white", "salmon")[as.factor(df[1,])], xaxt='n', yaxt = 'n', ylab= rownames(df)[1], border=NA)
+  if (unique(df[1,])==2) {
+    # binary phenotype
+    df <- df[,order(df["BMR",])]
+    barplot(rep(1,length(df[1,])), col=c("white", "salmon")[as.factor(df[1,])], xaxt='n', yaxt = 'n', ylab= rownames(df)[1], border=NA)
+  } else {
+    barplot(df[1,], xaxt='n', yaxt = 'n', ylab= rownames(df)[1], border=NA)
+  }
   for (i in 2:4){
     barplot(df[i,], col=mycolorsori[i], xaxt='n', yaxt = 'n', ylab= rownames(df)[i], border = mycolorsori[i])
   }
