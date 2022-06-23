4ca528a9797d97eca427bca11e9df1e2cdea873d
Jie Zhou <chowstat@gmail.com>
2022-06-23 16:33:53 -0400

    clean run_diffdriver.R

--- scripts/run_diffdriver.R
+++ scripts/run_diffdriver.R
@@ -1,20 +1,15 @@
 #!/usr/bin/env Rscript
 library(diffdriver)
-#library(fastTopics)
-#library(plyr)
 library(logging)
 drivermapsdir<- "~/SimingLab/library/diffdriver_anno/"
 outputdir <- "~/temp/"
 outputname <- "diffDriver_demo"
-load(paste0(drivermapsdir,"/mapsparameters/Fpars.rda"))  # see data-raw folder for how to generate Fpars from drivermaps results 
+load(paste0(drivermapsdir,"/mapsparameters/Fpars.rda"))  # see data-raw folder for how to generate Fpars from drivermaps results
 load(paste0(drivermapsdir,"/mapsparameters/BMRlist.rda")) # see data-raw folder for how to generate BMRlist from drivermaps results
 
 genef <- system.file("extdata/genes.txt", package = "diffdriver")
 mutf <- system.file("extdata/mutations.txt", package = "diffdriver")
 phenof <- system.file("extdata/phenotypes.txt", package = "diffdriver")
-#genef = paste0(drivermapsdir,"/extdata/genes.txt")
-#mutf = paste0(drivermapsdir,"/extdata/mutations.txt")
-#phenof =paste0(drivermapsdir, "/extdata/phenotypes.txt")
 
 logfile <- file(paste0(outputdir,"/", outputname, ".log"), open="wt")
 addHandler(writeToFile, file= logfile, level='DEBUG')
