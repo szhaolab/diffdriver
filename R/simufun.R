009791ace60bdbab40617ff71fad3aa7e101fa42
Jie Zhou <chowstat@gmail.com>
2022-08-02 16:51:12 -0400

    test that

--- R/simufun.R
+++ R/simufun.R
@@ -61,5 +61,6 @@ power_compare <- function(family, Niter, sgdata, Nsample,para,bmrpars,betaf0,bet
   }
   return(list("parameters"=a, "m1.pvalue" =m1.pvalue, "m2.pvalue" =m2.pvalue,"m3.pvalue" =m3.pvalue,"m4.pvalue" =m4.pvalue,
               "m5.pvalue" =m5.pvalue,"m6.pvalue" =m6.pvalue,
-              "m7.pvalue" =m7.pvalue,"m8.pvalue" =m8.pvalue))
+              "m7.pvalue" =m7.pvalue,"m8.pvalue" =m8.pvalue,"res.m1"=res.m1, "res.m2"=res.m2, "res.m3"=res.m3, "res.m4"=res.m4,
+              "res.m5"=res.m5, "res.m6"=res.m6, "res.m7"=res.m7, "res.m8"=res.m8))
 }
