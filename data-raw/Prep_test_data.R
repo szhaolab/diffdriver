812f8b710c52ec27a15c48b082b6982cb44c4f2f
Jie Zhou <chowstat@gmail.com>
2022-08-02 16:51:12 -0400

    test that

---
+++ data-raw/Prep_test_data.R
@@ -0,0 +1,17 @@
+set.seed(10)
+testdataBinary=power_compare(family="binary",Niter=1, sgdata=sgdata, bmrpars=log(BMR),
+                Nsample=50, betaf0=0,
+                beta_gc=c(1,Fe), par=c(0.8,0.2),
+                hotspot=c(0.9992105264,0.0007894736,0.9995310769,
+                          0.0004689231, 0.5935003662,0.4064996338,
+                          0.1473376680, 1.0000000000,
+                          8.4588061034, 1.0000000000))
+testdataCont=power_compare(family="cont",Niter=1, sgdata=sgdata, bmrpars=log(BMR),
+                             Nsample=50, betaf0=0,
+                             beta_gc=c(1,Fe), par=c(0,5,0.1,1),
+                             hotspot=c(0.9992105264,0.0007894736,0.9995310769,
+                                       0.0004689231, 0.5935003662,0.4064996338,
+                                       0.1473376680, 1.0000000000,
+                                       8.4588061034, 1.0000000000))
+usethis::use_data(testdataBinary, overwrite = T)
+usethis::use_data(testdataCont, overwrite = T)
