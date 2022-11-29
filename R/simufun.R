a4051a19ed87a01223f68edbe85dee7213d5ddc0
Jie Zhou <chowstat@gmail.com>
2022-11-29 16:25:22 -0500

    revised power_compare function

--- R/simufun.R
+++ R/simufun.R
@@ -35,29 +35,30 @@ power_compare <- function(binary, Niter, sgdata, Nsample,para,bmrpars,betaf0,bet
     fe1 <- c(ef$beta_gc[1], ef$beta_gc[1] + ef$beta_gc[2])[as.factor(funcv)]+hotsize
     fe2<- c(ef$avbetaf1, ef$avbetaf1 + ef$avbetaf2)[as.factor(funcv)]
     fe3 <- rep(ef$betaf1f2, length(funcv))
-    #fe4 <- rep(ef$avbetaf1f2, length(funcv))
+    fe4 <- rep(ef$avbetaf1f2, length(funcv))
     mr <- bmrmtx + ef$betaf0
     if (sum(mut) ==0) {next}
     res.m1 <- mlr(mut,e)
     res.m2 <- genefisher(mut,e_bisect)
     res.m3 <- genebinom(mut,e_bisect)
     res.m4 <- genelr(mut,e_bisect)
-    #res.m5 <-  ddmodel(mut,e, mr, fe1)
-    #res.m6<-   ddmodel(mut,e, mr, fe2)
+    res.m5 <-  ddmodel(mut,e, mr, fe1)
+    res.m6<-   ddmodel(mut,e, mr, fe2)
     res.m7 <-  ddmodel(mut,e, mr, fe3)
-    #res.m8 <-  ddmodel(mut,e, mr, fe4)
+    res.m8 <-  ddmodel(mut,e, mr, fe4)
     m1.pvalue[iter] <-  res.m1$pvalue
     m2.pvalue[iter] <-  res.m2$pvalue
     m3.pvalue[iter] <-  res.m3$pvalue
     m4.pvalue[iter] <-  res.m4$pvalue
-    #m5.pvalue[iter] <-  res.m5$pvalue
-    #m6.pvalue[iter] <-  res.m6$pvalue
+    m5.pvalue[iter] <-  res.m5$pvalue
+    m6.pvalue[iter] <-  res.m6$pvalue
     m7.pvalue[iter] <-  res.m7$pvalue
-    #m8.pvalue[iter] <-  res.m8$pvalue
+    m8.pvalue[iter] <-  res.m8$pvalue
     parameters=c(ef$beta_gc,ef$avbetaf1,ef$avbetaf2,ef$betaf1f2,ef$avbetaf1f2)
     a=rbind(a,parameters)
   }
    return(list("parameters"=a, "m1.pvalue" =m1.pvalue, "m2.pvalue" =m2.pvalue,
                "m3.pvalue" =m3.pvalue,"m4.pvalue" =m4.pvalue,
-                "m7.pvalue" =m7.pvalue))
+               "m5.pvalue" =m5.pvalue,"m6.pvalue" =m6.pvalue,
+                "m7.pvalue" =m7.pvalue,"m8.pvalue" =m8.pvalue))
   }
