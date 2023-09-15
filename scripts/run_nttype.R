f46261dbb79880c654aca8a4c13b1fc077b6dc12
jiezhou-2 <chowstat@gmail.com>
2023-09-15 17:37:51 -0400

    pkgdown

---
+++ scripts/run_nttype.R
@@ -0,0 +1,19 @@
+library("diffdriver")
+library("data.table")
+library("brglm")
+drivermapsdir <- "/dartfs/rc/lab/S/Szhao/jiezhou/driverMAPS/data/"
+outputname <- "temp/diffDriver_demo"
+i1=17
+i2=8
+typee=list.files("/dartfs/rc/lab/S/Szhao/diffDriver/data_run_prs/tumor_specific_input/")
+type=typee[i1]
+outputdir <- paste0("~/SimingLab/jiezhou/diffdriverPlaygournd/EMalgorithm/real/nttype/",type)
+mutationdir="/dartfs/rc/lab/S/Szhao/diffDriver/data_run_prs/tumor_specific_input/"
+genef = paste0(mutationdir,type,"/",type,"genes.txt")
+mutf = paste0(mutationdir,type,"/",type,"_mutations.txt")
+phenof =paste0(mutationdir,type,"/",type,"_PRS_46phenotype.txt")
+pheName=colnames(fread(phenof, header = "auto"))
+hotf=paste0(mutationdir,type,"/",type,"_hotpositions.txt")
+res <- diffdriver(genef, mutf, phenof,j=i2+4,hotf, drivermapsdir = drivermapsdir,k=6,mode=1, outputdir = outputdir, outputname = outputname)
+save(res,file=paste0(outputdir,"/Results_",type,"_",pheName[i2+4], ".Rd"))
+q("no")
---
+++ scripts/run_signature.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=cancertype_17_9
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=72:00:00
+#SBATCH --mem=16G
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=real.out
+#SBATCH --error=real.err
+time R CMD BATCH run_signature.R acancer_u7phenotype_9.out
