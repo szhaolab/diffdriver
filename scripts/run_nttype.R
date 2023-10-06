34d82efe7e9af60497906aba763515eaad84fd15
jiezhou-2 <chowstat@gmail.com>
2023-10-06 11:09:53 -0400

    revised scipts

--- scripts/run_nttype.R
+++ scripts/run_nttype.R
@@ -7,14 +7,16 @@ i1=17
 i2=9
 typee=list.files("/dartfs/rc/lab/S/Szhao/diffDriver/data_run_prs/tumor_specific_input/")
 type=typee[i1]
-outputdir <- paste0("~/Szhao/minghui/diffdriverPlaygournd/nttype/",type)
+outputdir <- paste0("~/SimingLab/jiezhou/diffdriverPlaygournd/EMalgorithm/real/nttype/",type)
 mutationdir="/dartfs/rc/lab/S/Szhao/diffDriver/data_run_prs/tumor_specific_input/"
 genef = paste0(mutationdir,type,"/",type,"genes.txt")
 mutf = paste0(mutationdir,type,"/",type,"_mutations.txt")
 phenof =paste0(mutationdir,type,"/",type,"_PRS_46phenotype.txt")
-pheName=colnames(fread(phenof, header = "auto"))
+bmrf=paste0(mutationdir,type,"/","BMRlist.Rd")
+
+pheName=colnames(data.table::fread(phenof, header = "auto"))
 hotf=paste0(mutationdir,type,"/",type,"_hotpositions.txt")
-res <- diffdriver(genef, mutf, phenof,j=i2+4,hotf, drivermapsdir = drivermapsdir,k=6,
+res <- diffdriver(genef, mutf, phenof,bmrf=bmrf,j=i2+4,hotf, drivermapsdir = drivermapsdir,k=6,
                   mode=1, outputdir = outputdir, outputname = outputname)
 save(res,file=paste0(outputdir,"/Results_",type,"_",pheName[i2+4], ".Rd"))
 q("no")
--- scripts/run_signature.R
+++ scripts/run_signature.R
@@ -1,7 +1,9 @@
 library("diffdriver")
+#library("devtools")
 library("brglm")
 library("fastTopics")
 library("data.table")
+#load("../")
 drivermapsdir <- "~/SimingLab/library/diffdriver_anno/"
 outputname <- "temp/diffDriver_demo"
 i1=17
@@ -15,7 +17,9 @@ mutf = paste0(mutationdir,type,"/",type,"_mutations.txt")
 phenof =paste0(mutationdir,type,"/",type,"_PRS_46phenotype.txt")
 pheName=colnames(fread(phenof, header = "auto"))
 hotf=paste0(mutationdir,type,"/",type,"_hotpositions.txt")
-res <- diffdriver(genef, mutf, phenof,j=i2+4,hotf, drivermapsdir = drivermapsdir,k=6,mode=2, outputdir = outputdir, outputname = outputname)
+bmrf=paste0(mutationdir,type,"/","BMRlist.Rd")
+
+res <- diffdriver(genef, mutf, phenof,bmrf,j=i2+4,hotf, drivermapsdir = drivermapsdir,k=6,mode=2, outputdir = outputdir, outputname = outputname)
 print(paste0(type,":",i2+4))
 save(res,file=paste0(outputdir,"/Results_",type,"_",pheName[i2+4],".Rd"))
 q("no")
